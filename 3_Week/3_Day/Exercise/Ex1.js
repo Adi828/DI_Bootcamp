7a0f15ddd17a02533baa53e58d2f4deb7a883027
Adi828 <Southerland828@gmail.com>
2021-01-20 11:11:39 +0200

    Update 20_01

---
+++ 3_Week/3_Day/Exercise/Ex1.js
@@ -0,0 +1,17 @@
+var age = prompt("whats your age?");
+
+console.log(age);
+
+if(age < 18) {
+
+	alert("too young");
+}
+
+else if (age==18) {
+	alert("enjoy");
+}
+
+else{
+	alert("power on!");
+}
+
