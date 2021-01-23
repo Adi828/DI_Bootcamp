5673cc6ec1c1063b876d8372f0e6e03f668ec375
Adi828 <Southerland828@gmail.com>
2021-01-23 16:27:17 +0200

    update

--- 3_Week/2_Day/Exercise/Guess.js
+++ 3_Week/2_Day/Exercise/Guess.js
@@ -1,7 +1,89 @@
 
 
-var x;
+var X = 5;
+var Y = "34";
 
-x = 3;
+var result = X + Y;
 
-console.log(x);
+console.log(result);
+ 
+var X = 5;
+var Y = "4";
+
+var result = X - Y;
+
+console.log(result);
+
+var X = true
+var Y = true
+
+var result = X + Y;
+
+console.log(result);
+
+var X = true
+var Y = false
+
+var result = X + Y;
+
+console.log(result);
+
+var X = false
+var Y = true
+
+var result = X + Y;
+
+console.log(result);
+
+var X = false
+var Y = true
+
+var result = X - Y;
+
+console.log(result);
+
+var X = 3
+var Y = 4
+
+var result = X - Y;
+
+console.log(result);
+
+
+ // - Subtraction
+
+//  var firstNumber = 21;
+
+//  var secondNumber = 4;
+
+//  var result = firstNumber - secondNumber;
+
+//  console.log(result);
+
+
+  // 5 + "34"
+  //   5 - "4"
+  //   10 % 5
+  //   5 % 10
+  //   "Java" + "Script"
+  //   " " + " "
+  //   " " + 0
+  //   true + true
+  //   true + false
+  //   false + true
+  //   false - true
+  //   3 - 4
+  //   "Bob" - "bill"
+
+
+
+  //Boolean
+//JavaScript has a Boolean data type. It can only take the values true or false
+
+//The Boolean() method: to find out if an expression (or a variable) is true
+
+//var op = Boolean(10 > 9)   // op returns true
+
+// var op = (10 > 9) 
+
+// console.log(op);
